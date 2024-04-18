40e7bcbad0551bd6a978b543fcacdd2807853897
KellyAineah <kellyainea99@gmail.com>
2024-04-18 21:57:50 +0300

    added a fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+      .then(response => response.json())
+      .then(data => renderImages(data.message));
+  });
+  
+  function renderImages(images) {
+    const imagesContainer = document.getElementById('dog-image-container');
+    images.forEach(imageUrl => {
+      let img = document.createElement('img');
+      img.src = imageUrl;
+      imagesContainer.appendChild(img);
+    });
+  }
+  //challenge 2
+  document.addEventListener('DOMContentLoaded', () => {
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    fetch(breedUrl)
+      .then(response => response.json())
+      .then(data => {
+        const breedList = document.getElementById('dog-breeds');
+        for (const breed in data.message) {
+          const li = document.createElement('li');
+          li.textContent = breed;
+          breedList.appendChild(li);
+        }
+      });
+  });
+  //challenge 3
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const breedList = document.getElementById('dog-breeds');
+    breedList.addEventListener('click', event => {
+      if (event.target.tagName === 'LI') {
+        event.target.style.color = 'orange';  
+      }
+    });
+  });
+  //challenge 4
+  document.addEventListener('DOMContentLoaded', () => {
+    const dropdown = document.getElementById('breed-dropdown');
+    dropdown.addEventListener('change', (event) => {
+      const selectedLetter = event.target.value;
+      const lis = document.querySelectorAll('#dog-breeds li');
+      lis.forEach(li => {
+        if (li.textContent.startsWith(selectedLetter)) {
+          li.style.display = 'list-item';
+        } else {
+          li.style.display = 'none';
+        }
+      });
+    });
+  });
+  
+  
